6605f6ea780556d8a41627b8022497276d5ab58a
LuuTuanNghiavtv <90957661+LuuTuanNghiavtv@users.noreply.github.com>
2021-11-08 09:12:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -32,6 +32,45 @@ menuTabs.addEventListener("click",function(e){
 
      }
 })
+const inputSearch = document.querySelector(".input-search")
+const autoBox = document.querySelector(".autobox")
+
+inputSearch.onkeyup = (e) => {
+    let checkData = e.target.value;
+    let dataArray = []
+    if (checkData) {
+        dataArray = recomentlist.filter((data)=>{
+           return data.toLocaleLowerCase().startsWith(checkData.toLocaleLowerCase())
+        })
+        dataArray = dataArray.map((data)=>{
+            return data = "<li>"+data+"</li>"
+        })
+        autoBox.classList.add("active")
+        showadress (dataArray)
+        let liItem = autoBox.querySelectorAll("li")
+        for (let i=0;i<liItem.length;i++){
+              liItem[i].addEventListener("click",function(){
+           inputSearch.value = liItem[i].innerHTML
+           autoBox.classList.remove("active")
+         })
+        }
+        }
+    else {
+        autoBox.classList.remove("active")
+    }
+  
+   
+}
+function showadress (list){
+    let listData
+    if(!list.length) {
+        listData = "<li>"+inputSearch.value+"</li>"
+    }
+    else {
+        listData = list.join("")
+    }
+    autoBox.innerHTML = listData
+}
 //-----------------Animation-----------
 AOS.init();
 

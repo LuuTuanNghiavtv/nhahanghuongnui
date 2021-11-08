56d935b07ba41ee49d66171a0619750edd268c9a
LuuTuanNghiavtv <90957661+LuuTuanNghiavtv@users.noreply.github.com>
2021-11-08 09:55:11 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,38 @@
-const Nav_tongger = document.querySelector(".nav-toggler");
-Nav_tongger.addEventListener("click", toggleNav);
-function toggleNav(){
-    document.querySelector(".nav-toggler").classList.toggle("active");
-    document.querySelector(".nav").classList.toggle("open");
-}
-document.addEventListener("click",function(a){
-    if(a.target.closest(".nav-item")){
-        toggleNav();
-    }
+const imgItem = document.querySelectorAll(".aspect-ratio-169 img")
+const imgItemContainer = document.querySelector(".aspect-ratio-169")
+const dotItem = document.querySelectorAll(".dot")
+let index = 0;
+let imgLeng = imgItem.length
+imgItem.forEach(function(image,index){
+    image.style.left = index*100 + "%"
+    dotItem[index].addEventListener("click",function(){
+    slideRun (index)
+    })
 })
+function slider (){
+    index++;
+    if(index >= imgLeng){index=0}
+  
+    slideRun (index)
+}
+function slideRun (index) {
+    imgItemContainer.style.left = "-" + index*100 + "%"
+    const dotActive = document.querySelector(".active")
+    dotActive.classList.remove("active")
+    dotItem[index].classList.add("active");
+}
+
+setInterval (slider,5000)
+
+
+
+
+const toP = document.querySelector(".top")
 window.addEventListener("scroll",function(){
-    // console.log(this.pageYOffset)
-    if(this.pageYOffset > 60){
-        document.querySelector(".header").classList.add("sticky");
-    }
-    else {
-        document.querySelector(".header").classList.remove("sticky")
-    }
-})
-const menuTabs = document.querySelector(".menu-taps") ;
-menuTabs.addEventListener("click",function(e){
-    if(e.target.classList.contains("menu-tap-item")&& !e.target.classList.contains("active"))
-    {
-        const target = e.target.getAttribute("data-target");
-        // console.log(target);
-        menuTabs.querySelector(".active").classList.remove("active");
-        e.target.classList.add("active");
-        const menuSection = document.querySelector(".menu-section");
-        menuSection.querySelector(".menu-tap-content.active").classList.remove("active");
-        menuSection.querySelector(target).classList.add("active");
-
-     }
-})
-const inputSearch = document.querySelector(".input-search")
-const autoBox = document.querySelector(".autobox")
-
-inputSearch.onkeyup = (e) => {
-    let checkData = e.target.value;
-    let dataArray = []
-    if (checkData) {
-        dataArray = recomentlist.filter((data)=>{
-           return data.toLocaleLowerCase().startsWith(checkData.toLocaleLowerCase())
-        })
-        dataArray = dataArray.map((data)=>{
-            return data = "<li>"+data+"</li>"
-        })
-        autoBox.classList.add("active")
-        showadress (dataArray)
-        let liItem = autoBox.querySelectorAll("li")
-        for (let i=0;i<liItem.length;i++){
-              liItem[i].addEventListener("click",function(){
-           inputSearch.value = liItem[i].innerHTML
-           autoBox.classList.remove("active")
-         })
-        }
-        }
-    else {
-        autoBox.classList.remove("active")
-    }
-  
-   
-}
-function showadress (list){
-    let listData
-    if(!list.length) {
-        listData = "<li>"+inputSearch.value+"</li>"
-    }
-    else {
-        listData = list.join("")
-    }
-    autoBox.innerHTML = listData
-}
-//-----------------Animation-----------
-AOS.init();
-
-  
-                        
+   const X = this.pageYOffset;
+ if(X>1){toP.classList.add("active")}
+ else {
+     toP.classList.remove("active")
+ }
+}) 
+                       
